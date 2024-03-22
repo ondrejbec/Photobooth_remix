b6cc469574efe68b6c282c283297b83ea7f619f7
ondrejbec <ondrejbec@gmail.com>
2024-03-22 11:43:27 +0100

    setup tailwind

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,5 +1,7 @@
 import type { MetaFunction } from "@remix-run/node";
 
+
+
 export const meta: MetaFunction = () => {
   return [
     { title: "New Remix App" },
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,20 @@
 import type { Config } from 'tailwindcss'
+import path from 'node:path';
+
+import utimaUi from '@utima/ui/plugin';
 
 export default {
-  content: ['./app/**/*.{js,jsx,ts,tsx}'],
+  content: [ `${path.dirname(require.resolve('@utima/ui'))}/**/*.js`],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require('tailwindcss-animate'), utimaUi({
+    colors: {
+      primary: {
+        bg: '#cc0000',
+        fg: '#ffffff',
+      },
+    }
+  })],
 } satisfies Config
 
