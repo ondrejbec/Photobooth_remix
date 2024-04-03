cd40c18a2bdb2d72370f4ff5dc44749d5c960e75
ondrejbec <ondrejbec@gmail.com>
2024-04-03 13:13:03 +0200

    add href to footer

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,9 +1,8 @@
-import { Link } from "lucide-react";
+
 
 
 export default function Footer() {
     return (
-    <footer className="bg-primary flex text-white p-4 justify-center"><Link to="www.utima.cz">Utima.cz</Link></footer>
-
+    <footer className="bg-primary flex text-white p-4 justify-center"><a href="https://utima.cz/">Utima.cz</a></footer>
     );
   }
