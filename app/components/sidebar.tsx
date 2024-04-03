3a7f9be7376a3e6fb3ecf2df7e2e5e6977c9115d
ondrejbec <ondrejbec@gmail.com>
2024-04-03 13:11:11 +0200

    add Links

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -1,13 +1,27 @@
 import { Button } from "./ui/ui/button"
+import { Link } from "@remix-run/react";
+
 
 
 export default function Sidebar() {
     return (
     <aside className="bg-gray-700 text-white w-64 p-4">
         <ul className="w-full">
-          <li className="p-2"><Button >Fotky</Button></li>
-          <li className="p-2"><Button>Historie</Button></li>
-          <li className="p-2"><Button>Kredity</Button></li>
+          <li className="p-2">
+            <Button>
+              <Link to="/photos">Fotky</Link>
+            </Button>
+          </li>
+          <li className="p-2">
+            <Button>
+              <Link to="">Galerie</Link>
+            </Button>
+          </li>
+          <li className="p-2">
+            <Button>
+              <Link to="">Nastaveni</Link>
+            </Button>
+          </li>
         </ul>
     </aside>
     );
