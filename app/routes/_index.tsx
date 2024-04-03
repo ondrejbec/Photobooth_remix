4651f74c9944d26994b8cb2c96231a5a23227eee
ondrejbec <ondrejbec@gmail.com>
2024-04-03 14:27:09 +0200

    add links

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,7 +1,6 @@
 import type { MetaFunction } from "@remix-run/node";
 import  Sidebar  from "~/components/sidebar"
-import Webcam from "react-webcam";
-import { Form } from "@remix-run/react";
+import { useState } from "react";
 
 export const meta: MetaFunction = () => {
   return [
@@ -9,11 +8,8 @@ export const meta: MetaFunction = () => {
     { name: "description", content: "Welcome to Remix!" },
   ];
 };
-
-
-
 export default function Index() {
-  
+  const [youtubeID] = useState('r4B69HAOXnA')
   return (
     <div className="flex flex-col h-screen">
     <div className="flex flex-1">
@@ -21,8 +17,23 @@ export default function Index() {
       <main className="bg-slate-500 flex-1 p-4">
         <div className="flex flex-row p-4">
           <div className="w-1/2">
-            <h1 className="text-white text-3xl font-semibold">Remix</h1>
-            <h2 className="text-white text-xl font-semibold">Links</h2>
+            <h1 className="text-white text-3xl font-semibold pb-2">Remix</h1>
+            <h2 className="text-white text-xl font-semibold pb-2">Links</h2>
+            <ul className="text-blue-300 flex flex-row text-lg gap-4 underline">
+              <li><a href="https://remix.run/docs/en/main">Documentation</a></li>
+              <li><a href="https://remix.run/docs/en/main">Official page</a></li>
+              <li><a href="https://www.youtube.com/@Remix-Run/videos">Youtube channel</a></li>
+              <li><a href="https://github.com/remix-run/remix">Github</a></li>
+            </ul>
+            <h2 className="text-white text-xl font-semibold py-2">Articles</h2>
+            <ul className="text-blue-300 flex flex-row text-lg gap-4 underline">
+              <li><a href="https://www.dhiwise.com/post/an-in-depth-analysis-remix-vs-react-which-one-is-supreme">Comparsion</a></li>
+            </ul>
+            <iframe width={650} height={500} className='pt-6 video'
+                title='Youtube player'
+                sandbox='allow-same-origin allow-forms allow-popups allow-scripts allow-presentation'
+                src={`https://youtube.com/embed/${youtubeID}?autoplay=0`}>
+            </iframe>
           </div>
         </div>
       </main>
